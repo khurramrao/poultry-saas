0d145d006f7f02dea6e5e68a670240c22c6bc767
khurramrao <raokhurram.zahid@gmail.com>
2026-05-01 03:26:36 +0500

    Create superuser automatically

--- farm_project/settings.py
+++ farm_project/settings.py
@@ -139,3 +139,9 @@ DATABASES = {
         conn_max_age=600,
     )
 }
+
+if os.environ.get("CREATE_SUPERUSER") == "True":
+    from django.contrib.auth import get_user_model
+    User = get_user_model()
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser("admin", "admin@test.com", "admin123")
